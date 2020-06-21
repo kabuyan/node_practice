c3732c1a6f369c746fd8c2184e16bf5a239f543e
kabuyan <kabuto64225@gmail.com>
2020-06-22 00:01:01 +0900

    Expressでのルーティング

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var http=require('http')
-var express=require('express')
+var http=require('http');
+var express=require('express');
 
-var app=express()
+var app=express();
 
-app.use(function(req,res,next){
-    return res.send('Hello World')
+app.get("/",function(req,res,next){
+    return res.send('Hello World');
+});
+
+app.get("/hoge",function(req,res,next){
+    return res.send('Hoge');
 });
 
 var server=http.createServer(app);
