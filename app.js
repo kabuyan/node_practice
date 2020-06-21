f3842b68a02edd8eecdeabff88edf4c46cc5efcd
kabuyan <kabuto64225@gmail.com>
2020-06-22 01:02:51 +0900

    Expressでのテンプレートエンジンの使用

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var http=require('http');
+var path=require('path');
 var express=require('express');
 
 var app=express();
 
-app.get("/",function(req,res,next){
-    return res.send('Hello World');
-});
+// テンプレートエンジンとして設定
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
 
-app.get("/hoge",function(req,res,next){
-    return res.send('Hoge');
+app.get("/",function(req,res,next){
+    // HTMLをレスポンスとして返す場合は'res.render'を使用する
+    // res.render(ファイル名,埋め込みたい変数)
+    return res.render('index',{ 
+        title: 'Hello World'
+    });
 });
 
 var server=http.createServer(app);
