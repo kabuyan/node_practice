b39e095c1944671929ce2c39d719ddb0527cbbc4
kabuyan <kabuto64225@gmail.com>
2020-06-23 01:03:34 +0900

    画像の添付

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path=require('path');
 var express=require('express');
 var bodyparser=require('body-parser');
 var mongoose=require('mongoose');
+var fileUpload=require('express-fileupload');
 
 // スキーマをインポート
 var Message=require('./schema/Message');
@@ -24,6 +25,9 @@ app.use(bodyparser());
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
 
+// 画像ファイルの設定
+app.use("/image",express.static(path.join(__dirname,'image')));
+
 app.get("/",function(req,res,next){
     // データを取得
     // find(クエリ,コールバック関数)
@@ -39,17 +43,35 @@ app.get("/update",function(req,res,next){
     return res.render('update');
 });
 
-app.post("/update",function(req,res,next){
-    // データを格納
-    var newMessege=new Message({
-        username: req.body.username,
-        message: req.body.message
-    });
-    // データを保存
-    newMessege.save((err)=>{
-        if(err) throw err;
-        return res.redirect("/");
-    });
+app.post("/update",fileUpload(),function(req,res,next){
+    if(req.files&&req.files.image){
+        // 保存先を指定
+        req.files.image.mv('./image/'+req.files.image.name,function(err){
+            if(err) throw err;
+            // データを格納
+            var newMessege=new Message({
+                username: req.body.username,
+                message: req.body.message,
+                image_path: '/image/'+req.files.image.name
+            });
+            // データを保存
+            newMessege.save((err)=>{
+                if(err) throw err;
+                return res.redirect("/");
+            });
+        });
+    }else{
+        // データを格納
+        newMessege=new Message({
+            username: req.body.username,
+            message: req.body.message
+        });
+        // データを保存
+        newMessege.save((err)=>{
+            if(err) throw err;
+            return res.redirect("/");
+        });
+    }
 });
 
 var server=http.createServer(app);
